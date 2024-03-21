01eb16f4563f58a5f53b2cf2dce1e4d82dd544df
PranaV Shimpi <23pranavshimpi@gmail.com>
2024-03-21 17:58:15 +0530

    basic structure created

--- 08-auth-o-authentication/src/App.jsx
+++ 08-auth-o-authentication/src/App.jsx
@@ -1,7 +1,27 @@
+import React from "react";
+import { BrowserRouter as Routes, Router, Route } from "react-router-dom";
+import SiteHeader from "./components/SiteHeader";
+import Dashboard from "./pages/Dashboard";
+import Home from "./pages/Home";
 import "./App.css";
 
-function App() {
-  return <div>hello</div>;
-}
+export default function App() {
+  return (
+    <Router>
+      <div className="app">
+        {/* site header */}
+        <SiteHeader />
 
-export default App;
+        {/* routes */}
+        <Routes>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="/" exact={true}>
+            <Home />
+          </Route>
+        </Routes>
+      </div>
+    </Router>
+  );
+}
