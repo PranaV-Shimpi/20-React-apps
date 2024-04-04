8a7c636579aa77cb1ffc300b17d3730a9c287294
PranaV Shimpi <23pranavshimpi@gmail.com>
2024-04-04 20:05:48 +0530

    added styled component to dates picker

--- 10-calendar-dates-picker/src/App.js
+++ 10-calendar-dates-picker/src/App.js
@@ -1,64 +1,150 @@
-import React, { useState } from "react";
-import "./App.css";
+import React, { useState } from 'react';
+import styled, { css } from 'styled-components';
+import './App.css';
 
 const calendarDates = Array(31)
   .fill(0)
   .map((e, i) => i);
 
 export default function App() {
-  const [choosingType, setChoosingType] = useState("start");
+  const [choosingType, setChoosingType] = useState('start'); // start or end
   const [startDate, setStartDate] = useState(null);
   const [endDate, setEndDate] = useState(null);
+  const [hoverDate, setHoverDate] = useState(null);
 
   function updateDate(chosenDay) {
+    // handle if a user chose before our current range
     if (startDate && chosenDay < startDate) {
       setStartDate(chosenDay);
-      return setChoosingType("end");
+      return setChoosingType('end');
     }
 
+    // handle if a user chose after our current range
     if (endDate && chosenDay > endDate) {
       setEndDate(chosenDay);
-      return setChoosingType("end");
+      return setChoosingType('end');
     }
 
-    if (choosingType === "start") {
+    if (choosingType === 'start') {
       setStartDate(chosenDay);
-      setChoosingType("end");
-      return;
+      return setChoosingType('end');
     }
-    if (choosingType === "end") {
+
+    if (choosingType === 'end') {
       setEndDate(chosenDay);
     }
   }
 
+  function checkInBetween(day) {
+    if (startDate && !endDate) return day > startDate && day < hoverDate;
+    return day > startDate && day < endDate;
+  }
+
   return (
-    <div className="body">
-      <div className="date-chooser">
-        <button
-          className="date-chooser-button"
-          onClick={setChoosingType("start")}
+    <>
+      <StyledDateChooser>
+        <StyledDateChooserButton
+          onClick={() => setChoosingType('start')}
+          isChoosing={choosingType === 'start'}
         >
           Start Date <span>{startDate}</span>
-        </button>
-        <button className="date-chooser-button">
+        </StyledDateChooserButton>
+        <StyledDateChooserButton
+          onClick={() => setChoosingType('end')}
+          isChoosing={choosingType === 'end'}
+        >
           End Date <span>{endDate}</span>
-        </button>
-      </div>
+        </StyledDateChooserButton>
+      </StyledDateChooser>
 
-      <div className="calendar">
+      <StyledCalendar>
         {calendarDates.map((day, index) => {
           const dayNumber = day + 1;
 
+          let isInBetween = checkInBetween(dayNumber);
+          let isSelected = dayNumber === startDate || dayNumber === endDate;
+
           return (
-            <button
-              className="calendar-day"
+            <StyledCalendarDay
+              key={index}
+              isInBetween={isInBetween}
+              isSelected={isSelected}
               onClick={() => updateDate(dayNumber)}
+              onMouseOver={() => setHoverDate(dayNumber)}
             >
               {dayNumber}
-            </button>
+            </StyledCalendarDay>
           );
         })}
-      </div>
-    </div>
+      </StyledCalendar>
+    </>
   );
 }
+
+const StyledDateChooser = styled.div`
+  display: flex;
+  margin-bottom: 20px;
+`;
+
+const StyledDateChooserButton = styled.button`
+  color: #0b204c;
+  text-transform: uppercase;
+  flex: 1;
+  padding: 15px;
+  background: none;
+  cursor: pointer;
+  border: none;
+  border-bottom: 2px solid rgba(11, 32, 76, 0.2);
+  outline: none;
+  border-color: ${(props) => (props.isChoosing ? '#0b204c' : 'none')};
+
+  span {
+    display: block;
+    min-height: 60px;
+    font-size: 50px;
+  }
+`;
+
+const StyledCalendar = styled.div`
+  max-width: 400px;
+  border-radius: 10px;
+  display: grid;
+  grid-template-columns: repeat(7, 1fr);
+  box-shadow: 0 20px 25px -5px rgba(0, 0, 0, 0.1),
+    0 10px 10px -5px rgba(0, 0, 0, 0.04);
+  background: #0b204c;
+  color: #fff;
+  padding: 20px;
+`;
+
+const StyledCalendarDay = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 10px;
+  transition: 0.3s ease background;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  color: #8096c1;
+  background: none;
+
+  ${(props) =>
+    props.isInBetween &&
+    css`
+      background: #254381 !important;
+      color: #eee;
+    `}
+
+  ${(props) =>
+    props.isSelected &&
+    css`
+      background: #1a1a1a !important;
+      color: #eee;
+    `}
+
+  &:hover {
+    color: #eee;
+    background: #254381;
+  }
+`;
--- 10-calendar-dates-picker/src/components/Banner.js
+++ 10-calendar-dates-picker/src/components/Banner.js
@@ -1,16 +1,27 @@
-import React from 'react';
-import './Banner.css';
+import React from "react";
+import "./Banner.css";
 
 const Banner = () => {
   return (
     <div className="banner">
       <div className="left">
-        <a href="/" className="project-name">🌟Calendar Date Picker</a>
+        <a href="/" className="project-name">
+          🌟Calendar Date Picker
+        </a>
       </div>
       <div className="right">
-      {/* eslint-disable-next-line */}
-        <a href="#" className="github-link">📄Source Code </a>
-        <a href="https://github.com/pranav-shimpi/20-React-apps/" className="all-projects-link">🚀20 React Projects </a>
+        <a
+          href="https://github.com/PranaV-Shimpi/20-React-apps/tree/main/10-calendar-dates-picker"
+          className="github-link"
+        >
+          📄Source Code{" "}
+        </a>
+        <a
+          href="https://github.com/pranav-shimpi/20-React-apps/"
+          className="all-projects-link"
+        >
+          🚀20 React Projects{" "}
+        </a>
       </div>
     </div>
   );
