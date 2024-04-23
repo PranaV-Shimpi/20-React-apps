d86f39206d3df80da0edbafd7803bccff949176f
PranaV Shimpi <23pranavshimpi@gmail.com>
2024-04-23 18:46:36 +0530

    14-file explorer: Initial Setup for

---
+++ 14-file-explorer/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     File Explorer
+    </div>
+  );
+}
+
+export default App;
---
+++ 14-file-explorer/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import Banner from "./components/banner/Banner";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Banner />
+    <App />
+  </React.StrictMode>
+);
