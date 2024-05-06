86cc52750b085aa1349c1d9cc365539a5d051a16
PranaV Shimpi <23pranavshimpi@gmail.com>
2024-05-06 18:43:26 +0530

    file-structure completed

--- 14-file-explorer/src/App.js
+++ 14-file-explorer/src/App.js
@@ -1,11 +1,22 @@
-import './App.css';
+import { useState } from "react";
+import Folder from "./components/Folder";
+import useTraverseTree from "./hooks/use-traverse-tree";
+import "./styles.css";
+import explorer from "./data/folderData"
 
-function App() {
+export default function App() {
+  const [explorerData, setExplorerData] = useState(explorer);
+
+  const { insertNode } = useTraverseTree();
+
+  const handleInsertNode = (folderId, item, isFolder) => {
+    const finalTree = insertNode(explorerData, folderId, item, isFolder);
+    setExplorerData(finalTree);
+  };
+
   return (
     <div className="App">
-     File Explorer
+      <Folder handleInsertNode={handleInsertNode} explorer={explorerData} />
     </div>
   );
 }
-
-export default App;
