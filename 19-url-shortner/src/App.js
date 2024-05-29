df185c25d75baa0ac7d4ffb549ad559a6e370dc5
PranaV Shimpi <23pranavshimpi@gmail.com>
2024-05-29 18:33:12 +0530

    19th project setup done

---
+++ 19-url-shortner/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Banner from "./components/Banner/Banner";
+
+function App() {
+  return (
+    <div>
+      <Banner />
+      URL SHORTNER
+    </div>
+  );
+}
+
+export default App;
